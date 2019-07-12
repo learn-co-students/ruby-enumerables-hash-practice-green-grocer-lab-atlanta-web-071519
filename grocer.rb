1c0fd72bb275e17390c65d860fafb09c5613b241
Danny Rooker <dannyrooks@gmail.com>
2019-07-12 11:05:00 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,23 @@
 def consolidate_cart(cart)
-  # code here
-end
+  item_list = {}
+  cart.each do |x|
+    x.each do |item, attribute|
+     if item_list.key?(item) == false
+       item_list[item] = attribute
+       item_list[item][:count] = 1
+      else
+       item_list[item][:count] += 1
+      end 
+    end
+  end
+  return item_list
+end  
+
 
 def apply_coupons(cart, coupons)
-  # code here
+  item_list = {}
+  cart.each do |x|
+    x.each do |key, value|
 end
 
 def apply_clearance(cart)
@@ -12,4 +26,15 @@ end
 
 def checkout(cart, coupons)
   # code here
-end
+
+
+
+
+
+
+
+
+
+
+
+
